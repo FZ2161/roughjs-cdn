90e4705380947d18db0ef72b8d87aa36e7b926e6
FZ2161 <filipzalewski44@gmail.com>
2024-04-24 10:35:21 +0200

    kwadrat do obrocenia

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 // https://github.com/rough-stuff/rough/wiki
 
 let roughSVG = rough.svg(document.getElementById("svg"))
-// const svg = document.getElementById()
-var globalY=140
-var globalX=300
-const line = roughSVG.line(0,0,100,100)
+
+
+
+// const line = roughSVG.line(0,0,100,100)
 // svg.appendChild(line)
 
 // const rect = roughSVG.rectangle(20,20,400,400,{
@@ -17,10 +17,18 @@ const line = roughSVG.line(0,0,100,100)
 // })
 // svg.appendChild(rect)
 
-///---  +, aby środek elipsy był w dobrym mscu   -, aby elipsa była wcześniej  ---///
-const ellipse = roughSVG.ellipse(globalX+150,globalY+80-160,300,100)
+var globalY=140
+var globalX=300
+//!###  +, aby środek elipsy był w dobrym mscu   -, aby elipsa była wcześniej  ###!///
+const ellipse = roughSVG.ellipse(globalX+150,globalY+80-160,300,100,{
+    fill: 'rgba(0,50,200,0.6)',
+    hachureAngle: 6, // angle of hachure,
+    hachureGap: 4  
+})
 svg.appendChild(ellipse)
-
+//line
+const line = roughSVG.line(globalX+150,globalY+50-80,globalX+150,globalY+50+30-80)
+svg.appendChild(line)
 
 const buttonAdd = document.querySelector("#addRect")
 buttonAdd.addEventListener("click", ()=>{
@@ -32,11 +40,34 @@ buttonAdd.addEventListener("click", ()=>{
 function drawRect(x,y){
     const rect = roughSVG.rectangle(x,y,300,100,{
         fill: 'rgba(255,0,200,0.6)',
-        fillStyle: 'zigzag-line' // solid fill   
+        fillStyle: 'zigzag-line'    
     })
     rect.addEventListener("click", ()=>{
         console.log("ok")
     })
     svg.appendChild(rect)
     globalY = globalY+130
+
+    const line = roughSVG.line(globalX+150,globalY+50-80,globalX+150,globalY+50+30-80)
+    svg.appendChild(line)
+}
+
+
+const addDiamond = document.querySelector("#addDiamond")
+addDiamond.addEventListener("click", ()=>{
+    console.log(100/Math.sqrt(2))
+    drawDiamond(globalX,globalY)
+})
+
+function drawDiamond(x,y){
+    x=y=1
+    const test = roughSVG.rectangle(globalX+150,globalY+50+30-80,100/Math.sqrt(2),100/Math.sqrt(2),{
+        fill: 'rgba(255,0,200,0.6)',
+        fillStyle: 'zigzag-line'    
+    })
+    console.log("100/Math.sqrt(2)")
+    svg.appendChild(test)
+    globalY = globalY+130
+    const line = roughSVG.line(globalX+150,globalY+50-80,globalX+150,globalY+50+30-80)
+    svg.appendChild(line)
 }
